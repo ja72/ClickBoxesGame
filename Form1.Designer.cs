eeaf79fbba0e2f9a1ef852a8ffae6099bdb3903a
John Alexiou <jalexiou@hotmail.com>
2021-06-02 08:50:43 -0400

    Code import
    https://stackoverflow.com/a/67739315/380384

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.PnlGameField = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1,
+            this.toolStripButton1,
+            this.toolStripTextBox2,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(99, 22);
+            this.toolStripLabel1.Text = "Number of Boxes";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(60, 25);
+            this.toolStripTextBox1.Text = "12";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(35, 22);
+            this.toolStripButton1.Text = "Start";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.toolStripTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.ReadOnly = true;
+            this.toolStripTextBox2.Size = new System.Drawing.Size(200, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(39, 22);
+            this.toolStripLabel2.Text = "Score:";
+            // 
+            // PnlGameField
+            // 
+            this.PnlGameField.BackColor = System.Drawing.Color.White;
+            this.PnlGameField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PnlGameField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlGameField.Location = new System.Drawing.Point(0, 25);
+            this.PnlGameField.Name = "PnlGameField";
+            this.PnlGameField.Size = new System.Drawing.Size(800, 425);
+            this.PnlGameField.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PnlGameField);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Panel PnlGameField;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static Random rnd = new Random();
+
+        readonly GameScore score = new GameScore();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            timer1.Enabled = false;
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                EndGame();
+            }
+            else
+            {
+                StartGame();
+            }
+        }
+
+        private void StartGame()
+        {
+            toolStripButton1.Text = "Stop";
+            toolStripButton1.BackColor = Color.FromArgb(255, 128, 128);
+            if (int.TryParse(toolStripTextBox1.Text, out int count) && count > 0)
+            {
+                PnlGameField.Controls.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    AddRandomBox();
+                }
+                score.Reset();
+                toolStripTextBox2.Text = score.ToString();
+                timer1.Enabled = true;
+            }
+        }
+        private void EndGame()
+        {
+            toolStripButton1.Text = "Start";
+            toolStripButton1.BackColor = Color.FromArgb(128, 255, 128);
+            timer1.Interval = 500;
+            timer1.Enabled = false;
+        }
+
+        private bool AddRandomBox()
+        {
+            var boxes = PnlGameField.Controls.OfType<PictureBox>().ToList();
+            if (CheckCollisions(boxes, out int indexA, out int indexB))
+            {
+                return false;
+            }
+            //Object Box is created with 20 Pixels height and width and a random color is generated for their background
+            var picBox = new PictureBox();
+            picBox.Size = new Size(20, 20);
+            picBox.BackColor = Color.FromArgb(rnd.Next(10, 245), rnd.Next(10, 245), rnd.Next(10, 245));
+
+            //Event handler will be added to click the box event 
+            picBox.Click += PicBox_Click;
+
+            //Box will be added to the game field with 30 Pixels distance to the edges
+            boxes.Add(picBox);
+            do
+            {
+                picBox.Location = new Point(rnd.Next(0, PnlGameField.Width - 30), rnd.Next(0, PnlGameField.Height - 30));
+            } while (CheckCollisions(boxes, out _, out _));
+            PnlGameField.Controls.Add(picBox);
+            return true;
+        }
+
+        private void PicBox_Click(object sender, EventArgs e)
+        {
+            var target = (PictureBox)sender;
+            if (timer1.Enabled)
+            {
+                RemoveBox(target);
+            }
+        }
+        private void RemoveBox(PictureBox box)
+        {
+            score.Add(box.Width);
+            box.Dispose();
+            PnlGameField.Controls.Remove(box);
+            if (!AddRandomBox())
+            {
+                EndGame();
+            }
+            toolStripTextBox2.Text = score.ToString();
+            PnlGameField.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (rnd.Next(1000) < timer1.Interval-1)
+            {
+                timer1.Interval += 1;
+            }
+            var boxes = PnlGameField.Controls.OfType<PictureBox>().ToArray();
+            foreach (var box in boxes)
+            {
+                box.Size = new Size(box.Size.Width + 2, box.Size.Height + 2);
+                box.Location = new Point(box.Location.X - 1, box.Location.Y - 1);
+            }
+            if (CheckCollisions(boxes, out int indexA, out int indexB))
+            {
+                if (indexA >= 0)
+                {
+                    boxes[indexA].BorderStyle = BorderStyle.FixedSingle;
+                }
+                if (indexB >= 0)
+                {
+                    boxes[indexA].BorderStyle = BorderStyle.FixedSingle;
+                }
+                EndGame();
+            }
+            PnlGameField.Invalidate();
+        }
+        private bool CheckCollisions()
+            => CheckCollisions(PnlGameField.Controls.OfType<PictureBox>().ToArray(), out _, out _);
+
+        private bool CheckCollisions(IList<PictureBox> boxes, out int indexA, out int indexB)
+        {
+            
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                var box = boxes[i];
+                indexA = i;
+                if (box.Left < 0 || box.Right >= PnlGameField.Width
+                    || box.Top < 0 || box.Bottom >= PnlGameField.Height)
+                {
+                    indexB = -1;
+                    return true;
+                }
+                for (int j = i+1; j < boxes.Count; j++)
+                {
+                    indexB = j;
+                    var other = boxes[j];
+                    if (box.Bounds.IntersectsWith(other.Bounds))
+                    {
+                        return true;
+                    }
+                }
+            }
+            indexA = -1;
+            indexB = -1;
+            return false;
+        }
+    }
+
+    public class GameScore
+    {
+        readonly List<float> pointsList;
+        public GameScore()
+        {
+            pointsList = new List<float>();
+        }
+        public int BoxCount { get => pointsList.Count; }
+        public float Average { get => pointsList.Count >0 ? pointsList.Average() : 0; }
+        public float Maximum { get => pointsList.Count>0 ? pointsList.Max() : 0; }
+        public float Total { get => pointsList.Count>0 ? pointsList.Sum() : 0; }
+
+        public void Add(int points)
+        {
+            pointsList.Add(points);
+        }
+        public void Reset()
+        {
+            pointsList.Clear();
+        }
+
+        public override string ToString()
+        {
+            return $"Total={Total}, Ave={Average}, Max={Maximum}";
+        }
+    }
+}
